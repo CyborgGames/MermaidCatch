5f3f035e76de4808d42356de969849cd85edc18b
Cidney Hamilton <cidney@cidneyhamilton.com>
2019-10-19 22:04:13 -0400

    Play sound effect when bouncing off a spear.

---
+++ Assets/Scripts/Spear.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MermaidCatch {
+
+	public class Spear : MonoBehaviour
+	{
+		
+		void OnCollisionEnter2D(Collision2D other) {
+			Debug.Log("Collision");
+			if (other.gameObject.tag == "Ball") {
+				Debug.Log("Spear collided with ball.");
+				AudioEvents.PlaySound("plop");
+			}
+		}
+	}
+}
