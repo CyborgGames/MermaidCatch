cd42b37de1594e848eb093b902b6dc1a1cfd35eb
Cidney Hamilton <cidney@cidneyhamilton.com>
2019-12-30 12:18:21 -0500

    Add animations to the seaweed and intro text; improve display of pixel art.

---
+++ Assets/Scripts/Blink.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MermaidCatch {
+
+	// Blink text back and forth
+	public class Blink : MonoBehaviour
+	{
+
+		// The text to blink
+		Text text;
+
+		public float startDelay = 0.1f;
+		public float interval = 1f;
+		
+		void Start() {
+			text = GetComponent<Text>();
+			InvokeRepeating("BlinkCycle", startDelay, interval);
+		}
+		
+		void BlinkCycle() {
+			text.enabled = !text.enabled;
+		}
+	}
+
+}
---
+++ Assets/Scripts/Sway.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MermaidCatch {
+	
+	public class Sway : MonoBehaviour
+	{
+
+		public float Range = 0.2f;
+		public float Speed = 5.0f;
+	   
+		void Start()
+		{
+			Speed = Random.Range(Speed - 2, Speed);
+			
+			// Initial rotation
+			transform.Rotate(Vector3.back * Range * Mathf.Sin(Random.Range(0f, 2f) * Speed));
+		}
+		
+		// Rotate back and forth;
+		void Update() {
+			transform.Rotate(Vector3.forward * Range * Mathf.Sin(Time.timeSinceLevelLoad * Speed));
+		}
+		
+	}
+}
+	
