1be4e46d64ba27b825ab964b7276c084b5111144
Cidney Hamilton <cidney@cidneyhamilton.com>
2020-01-30 23:20:57 -0500

    Because this game targets WebGL, don't bother handling quits.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,97 +4,92 @@ using UnityEngine;
 using Cyborg.Scenes;
 
 namespace MermaidCatch {
-
-	public class GameManager : Singleton<GameManager> {
-		
-		public static bool IsMenu = true;
-		
-		void OnEnable() {
-			UIEvents.OnStartGame += Reset;
-		}
-		
-		void OnDisable() {
-			UIEvents.OnStartGame -= Reset;
-		}
-
-		void Update () {
-			
-			// Pauses or plays game when player hits p
-			if (PressedPause()) {
-				Time.timeScale = IsPaused() ? 1 : 0;
-				UIEvents.Pause();
-			}
-
-			if (PressedQuit()) {
-				if (IsMenu || IsPaused()) {
-					Application.Quit();
-				}
-			}
-		}
-
-		public static void Win() {
-			AudioController.PlayWin();
-
-			SceneEvents.ChangeScene("_Win");
-			
-			Instance.GameOver();
-		}
-
-		public static void Lose() {
-			AudioController.PlayLose();
-
-			SceneEvents.ChangeScene("_Lose");
-			
-			Instance.GameOver();
-		}
-
-		public void UnPause() {
-			Time.timeScale = 1;
-		}
     
-		public void Pause() {
-			Time.timeScale = 0;
-		}
-
-		void Reset() {
-			IsMenu = false;
-			UnPause();
-		}
-		
-		void GameOver() {
-			StartCoroutine(ToMainMenu());
-		}
-
-
-		IEnumerator ToMainMenu() {
-
-			IsMenu = true;
-			
-			// AudioController.Pause();
-						
-			yield return new WaitForSeconds(3.0f);			
-
-			SceneEvents.ChangeScene("Title");
-
-			yield return new WaitForSeconds(1.5f);
-			
-			// AudioController.Restart();
-		}
-
-		
-		// Returns true if the player pressed the Pause button or key
-		bool PressedPause() {
-			return Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Pause);
-		}
-
-		bool PressedQuit() {
-			return Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.Escape);
-		}
-		
-		bool IsPaused() {
-			return Time.timeScale == 0;
-		}
-		
+    public class GameManager : Singleton<GameManager> {
+	
+	public static bool IsMenu = true;
+	
+	void OnEnable() {
+	    UIEvents.OnStartGame += Reset;
 	}
+	
+	void OnDisable() {
+	    UIEvents.OnStartGame -= Reset;
+	}
+	
+	void Update () {
+	    
+	    // Pauses or plays game when player hits p
+	    if (PressedPause()) {
+		Time.timeScale = IsPaused() ? 1 : 0;
+		UIEvents.Pause();
+	    }
+	    
+	}
+	
+	public static void Win() {
+	    AudioController.PlayWin();
+	    
+	    SceneEvents.ChangeScene("_Win");
+	    
+	    Instance.GameOver();
+	}
+	
+	public static void Lose() {
+	    AudioController.PlayLose();
+	    
+	    SceneEvents.ChangeScene("_Lose");
+	    
+	    Instance.GameOver();
+	}
+
+	public void UnPause() {
+	    Time.timeScale = 1;
+	}
+	
+	public void Pause() {
+	    Time.timeScale = 0;
+	}
+	
+	void Reset() {
+	    IsMenu = false;
+	    UnPause();
+	}
+	
+	void GameOver() {
+	    StartCoroutine(ToMainMenu());
+	}
+
+	
+	IEnumerator ToMainMenu() {
+	    
+	    IsMenu = true;
+	    
+	    // AudioController.Pause();
+	    
+	    yield return new WaitForSeconds(3.0f);			
+	    
+	    SceneEvents.ChangeScene("Title");
+	    
+	    yield return new WaitForSeconds(1.5f);
+	    
+	    // AudioController.Restart();
+	}
+	
+	
+	// Returns true if the player pressed the Pause button or key
+	bool PressedPause() {
+	    return Input.GetKeyDown(KeyCode.P) || Input.GetKeyDown(KeyCode.Pause);
+	}
+	
+	bool PressedQuit() {
+	    return Input.GetKeyDown(KeyCode.Q) || Input.GetKeyDown(KeyCode.Escape);
+	}
+	
+	bool IsPaused() {
+	    return Time.timeScale == 0;
+	}
+	
+    }
 
 }
--- Assets/Scripts/UI/TitleScreen.cs
+++ Assets/Scripts/UI/TitleScreen.cs
@@ -4,29 +4,29 @@ using Cyborg.Scenes;
 
 namespace MermaidCatch {
 
-	public class TitleScreen : MonoBehaviour {
-
-		// Start a new single player game
-		public void StartSinglePlayer() {
-			AudioController.PlayClick();
-			SceneEvents.ChangeScene("SinglePlayer");
-			UIEvents.StartGame();
-
-		}
-
-		// Start a new multiplayer game
-		public void StartMultiPlayer() {
-			AudioController.PlayClick();			
-			SceneEvents.ChangeScene("MultiPlayer");
-			UIEvents.StartGame();			
-		} 
-
-		// Also start when pressing Space
-		void Update() {
-			if (Input.GetKeyDown(KeyCode.Space)) {
-				StartSinglePlayer();
-			}
-		}
+    public class TitleScreen : MonoBehaviour {
+	
+	// Start a new single player game
+	public void StartSinglePlayer() {
+	    AudioController.PlayClick();
+	    SceneEvents.ChangeScene("SinglePlayer");
+	    UIEvents.StartGame();
+	    
 	}
-
+	
+	// Start a new multiplayer game
+	public void StartMultiPlayer() {
+	    AudioController.PlayClick();			
+	    SceneEvents.ChangeScene("MultiPlayer");
+	    UIEvents.StartGame();			
+	} 
+	
+	// Also start when pressing Space
+	void Update() {
+	    if (Input.GetKeyDown(KeyCode.Space)) {
+		StartSinglePlayer();
+	    }
+	}
+    }
+    
 }
